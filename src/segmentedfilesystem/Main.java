cc871a4f3d9e82b1f3365ad6742a788d90ef0c18
hamme503 <hamme503@morris.umn.edu>
2017-12-10 19:59:24 -0600

    Fix errors in implementation, TODO: Fix file writing error

--- src/segmentedfilesystem/Main.java
+++ src/segmentedfilesystem/Main.java
@@ -4,6 +4,8 @@ import java.nio.ByteBuffer;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
+import java.util.Iterator;
+import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.net.DatagramPacket;
@@ -16,10 +18,12 @@ public class Main {
 
     public static void main(String[] args) throws UnknownHostException, SocketException {
 
-    	DatagramSocket socket = new DatagramSocket(6014, InetAddress.getByName("146.57.33.55"));
+    	InetAddress target = InetAddress.getByName("146.57.33.55");
+    	int server_port = 6014;
+    	DatagramSocket socket = new DatagramSocket();
 
     	byte[] emptyBuf = new byte[0];
-    	DatagramPacket emptyPacket = new DatagramPacket(emptyBuf, 0);
+    	DatagramPacket emptyPacket = new DatagramPacket(emptyBuf, 0, target, server_port);
 
     	try {
     		socket.send(emptyPacket);
@@ -28,8 +32,10 @@ public class Main {
     		System.out.println("IOException when starting connection");
 
     	}
+    	
+    	byte[] buf = new byte[256];
 
-    	DatagramPacket p = null;
+    	DatagramPacket p = new DatagramPacket(buf, buf.length);
     	try {
 			socket.receive(p);
 		} catch (IOException e) {
@@ -43,11 +49,19 @@ public class Main {
     	ArrayList<DatagramPacket> headerArray = new ArrayList<DatagramPacket>();
     	ArrayList<Byte> IDArray = new ArrayList<Byte>();
 
+    	
+    	
     	while (true) {
     		if (p != null) {
     			packetArray.add(p);
     			if (isHeader(p)) {
     				headerArray.add(p);
+    				byte[] testb = new byte[p.getData().length - 2];
+    				for (int i = 2; i < testb.length; i++) {
+    					testb[i - 2] = p.getData()[i];
+    				}
+    				System.out.println(new String(testb));
+    				System.out.println("^ is header filename");
     				headerCount++;
     				packetCount++;
     			}
@@ -64,6 +78,8 @@ public class Main {
     			break;
     		}
     		try {
+    			buf = new byte[256];
+    			p = new DatagramPacket(buf, buf.length);
     			socket.receive(p);
     		} catch (IOException e) {
     			e.printStackTrace();
@@ -81,45 +97,57 @@ public class Main {
     	
     }
 
-//    public boolean isHeader(byte[] packetData) {
-//    	byte statusByte = packetData[0];
-//
-//    	if ((statusByte % 2) == 0) {
-//    		if ((statusByte % 4) == 3) {
-//    			return false;
-//    		}
-//    		return true;
-//    	}
-//    	return false;
-//    }
-//
-//    public boolean isFooter(byte[] packetData) {
-//    	byte statusByte = packetData[0];
-//
-//    	if ((statusByte % 4) == 3 || (statusByte % 4) == -1) {
-//
-//    		return true;
-//
-//    	}
-//    	return false;
-//
-//    }
-//
-//    public int getPacketNumber(byte[] packetData) {
-//
-//    	byte[] packetNumber = new byte[4];
-//    	byte[] b = packetData;
-//    	packetNumber[0] = (byte)0;
-//    	packetNumber[1] = (byte)0;
-//    	packetNumber[2] = b[2];
-//    	packetNumber[3] = b[3];
-//
-//    	return ByteBuffer.wrap(packetNumber).getInt();
-//
-//
-//    }
+    public boolean isHeader(byte[] packetData) {
+    	byte statusByte = packetData[0];
 
-    public ArrayList<ArrayList<TestPacket>> assembleFiles(ArrayList<TestPacket> data, ArrayList<TestPacket> files) {
+    	if ((statusByte % 2) == 0) {
+    		if ((statusByte % 4) == 3) {
+    			return false;
+    		}
+    		return true;
+    	}
+    	return false;
+    }
+
+    public boolean isFooter(byte[] packetData) {
+    	byte statusByte = packetData[0];
+
+    	if ((statusByte % 4) == 3 || (statusByte % 4) == -1) {
+
+    		return true;
+
+    	}
+    	return false;
+
+    }
+    
+    
+    public int testgetPacketNumber(byte[] packetData) {
+
+    	byte[] packetNumber = new byte[4];
+    	byte[] b = packetData;
+    	packetNumber[0] = (byte)0;
+    	packetNumber[1] = (byte)0;
+    	packetNumber[2] = b[2];
+    	packetNumber[3] = b[3];
+
+    	return ByteBuffer.wrap(packetNumber).getInt();
+
+
+    }
+    
+    public void testfilterData(ArrayList<TestPacket> data) {
+    	Iterator<TestPacket> packetIter = data.listIterator();
+    	ArrayList<TestPacket> filteredData = new ArrayList<>();
+    	while (packetIter.hasNext()) {
+    		if (isHeader(packetIter.next().getData())) {
+    			packetIter.remove();
+    		}
+    	}
+    	filteredData = data;
+    }
+    
+    public ArrayList<ArrayList<TestPacket>> testassembleFiles(ArrayList<TestPacket> data, ArrayList<TestPacket> files) {
     	ArrayList<ArrayList<TestPacket>> assembledFiles = new ArrayList<>();
       for (int i = 0; i < files.size(); i++) {
         ArrayList<TestPacket> currentFile = new ArrayList<>();
@@ -127,18 +155,25 @@ public class Main {
     		for (TestPacket currentPacket : data) {
     			if (((int) currentPacket.getData()[1]) == currentFileID) {
     				currentFile.add(currentPacket);
+    				System.out.println("stored" + currentPacket.toString());
     			}
     		}
+    		assembledFiles.add(currentFile);
     	}
+
+    	return assembledFiles;
     }
 
     private static void filterData(ArrayList<DatagramPacket> data) {
+    	
+    	Iterator<DatagramPacket> packetIter = data.listIterator();
     	ArrayList<DatagramPacket> filteredData = new ArrayList<>();
-    	for (DatagramPacket i : data) {
-    		if (isHeader(i)) {
-    			data.remove(i);
-    		}0
+    	while (packetIter.hasNext()) {
+    		if (isHeader(packetIter.next())) {
+    			packetIter.remove();
+    		}
     	}
+    	filteredData = data;
     }
 
     private static ArrayList<ArrayList<DatagramPacket>> assembleFiles(ArrayList<DatagramPacket> data, ArrayList<DatagramPacket> files) {
@@ -151,6 +186,7 @@ public class Main {
     				currentFile.add(currentPacket);
     			}
     		}
+    		assembledFiles.add(currentFile);
     	}
     	return assembledFiles;
     }
@@ -178,10 +214,19 @@ public class Main {
     	}
 
     	byte[] b = new byte[byteList.size()];
-    	byte[] h = new byte[fileNameByteList.size()]
-        		for (int i = 4; i < p.getData().length; i++) {;
+    	byte[] h = new byte[fileNameByteList.size()];
+    	
+    	for (int i = 0; i < byteList.size(); i++) {
+    		b[i] = byteList.get(i);
+    	}
+    	
+    	for (int i = 0; i < fileNameByteList.size(); i++) {
+    		h[i] = fileNameByteList.get(i);
+    	}
+
 
     	String fileName = new String(h);
+    	System.out.println("File name is:" + fileName);
 
     	//Credit for this file to StackOverflow question
     	//https://stackoverflow.com/questions/4350084/byte-to-file-in-java
@@ -196,11 +241,25 @@ public class Main {
     	//https://stackoverflow.com/users/432294/jay-sullivan
     	//https://stackoverflow.com/users/57611/erike
 
-    	try (FileOutputStream o = new FileOutputStream("./" + fileName)) {
+    	//FileUtils.writeByteArrayToFile(new File("/" + fileName), myByteArray);
+    	
+    	File file0 = new File("src/" + fileName);
+    	System.out.println(file0.getAbsolutePath());
+    	
+    	try {
+    	if (!file0.exists()) {
+    		file0.createNewFile();
+    	}
+    	} catch (IOException e) {
+    		System.out.println("God fucking damnit");
+    	}
+    	
+    	try (FileOutputStream o = new FileOutputStream(file0.getAbsolutePath())) {
     		o.write(b);
     		o.close();
     	} catch (IOException e) {
     		System.out.println("Error when writing to file");
+    		e.printStackTrace();
     	}
 
     }
@@ -287,5 +346,7 @@ public class Main {
 		}
 
 	}
+
+	
 
 }
--- src/segmentedfilesystem/TestPacket.java
+++ src/segmentedfilesystem/TestPacket.java
@@ -1,7 +1,13 @@
+package segmentedfilesystem;
+
 public class TestPacket {
   private byte[] data;
 
   public TestPacket(byte[] data) {
     this.data = data;
+  }
+
+  public byte[] getData() {
+    return data;
   }
 }
